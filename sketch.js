0a7b8b899558f8c71ed6acb2e3a9c96893cfe41c
martemm84 <104801193+martemm84@users.noreply.github.com>
2022-06-13 19:30:54 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	Matter.Body.rotate(rotator1,angle1),
+push();
+Translate(rotator1,position.x,rotator1.position.y);
+rotate(angle1);
+rect(0,0,150,20);
+Pop();
+angle1 +=0.2
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
+}
+
+
+
